0247b18b5a7bcd278d48c1da998251c26b9897b5
BearChoi <samuelys@naver.com>
2022-11-30 23:46:45 +0900

    add code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UserList from "./components/Users/UserList";
+
+
+function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevUSersList)=>{
+            return [
+                ...prevUSersList,
+                {name: userName, age: userAge, id: Math.random().toString()}
+            ];
+        });
+    }
+
+    return (
+        <>
+            <AddUser onAddUser={addUserHandler} />
+            <UserList users={usersList} />
+        </>
+    );
+}
+
+export default App;
